ec172c326829cac21d1751eba09449d4a725b546
Geraldo Medeiros <geraldo_sjs@hotmail.com>
2019-11-15 13:54:25 -0300

    including initial treatment for 404 error

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from .models import Question
-# Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
+from django.shortcuts import render
 
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+'''
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
@@ -11,6 +18,7 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+'''
 
 '''segunda forma
 def index(request):
@@ -23,7 +31,11 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 '''
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request,'polls/detail.html',{'quesyion':question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
