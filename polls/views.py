18b547ce2262f74038298c91be17eaf386925b5a
Geraldo Medeiros <geraldo_sjs@hotmail.com>
2019-11-17 14:11:22 -0300

    Using generic views

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.template import loader
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question, Choice
+from .models import Choice, Question
+
 
 """#index estático simples
 def index(request):
@@ -28,12 +28,12 @@ def index(request):
     return HttpResponse(template.render(context, request))
 """
 
-# reescrita do index interativo
+"""# reescrita do index interativo
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
-
+"""
 
 """#reescrita da view datail
 def detail(request, question_id):
@@ -44,36 +44,47 @@ def detail(request, question_id):
     return render(request,'polls/detail.html',{'quesyion':question})
 """
 
-# forma mais simplificada de levantar um erro 404 (Django)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+"""# forma mais simplificada de levantar um erro 404 (Django)
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
+"""
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
+"""
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+"""
 
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(
-            request,
-            "polls/detail.html",
-            {"question": question, "error_message": "You didn't select a choice.",},
-        )
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
     else:
         selected_choice.votes += 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
