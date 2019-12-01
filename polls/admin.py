982778d2c328cbadf5391b3c2eaa2e250d649816
Geraldo Medeiros <geraldo_sjs@hotmail.com>
2019-12-01 16:09:44 -0300

    Completing the Django Tutorial - Application Admin Site

--- polls/admin.py
+++ polls/admin.py
@@ -13,6 +13,8 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
 
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "tutorial.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -97,7 +97,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
 TIME_ZONE = "UTC"
 
